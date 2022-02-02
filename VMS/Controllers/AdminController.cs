a843d05b1d2beed503da5366b7233221b768fe8f
AbdulRafayArkitektz <85223854+AbdulRafayArkitektz@users.noreply.github.com>
2022-02-02 20:31:08 +0500

    new work

--- VMS/Controllers/AdminController.cs
+++ VMS/Controllers/AdminController.cs
@@ -15,6 +15,8 @@ using System.IO;
 using System.Drawing.Imaging;
 using System.Net;
 using CoreHtmlToImage;
+using VMS.Dtos;
+using VMS.Extensions;
 
 namespace VMS.Controllers
 {
@@ -30,12 +32,10 @@ namespace VMS.Controllers
             _context = context;
         }
 
-
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult LoadData()
         {
              
@@ -73,7 +73,9 @@ namespace VMS.Controllers
                                            appointment.PhoneNumber,
                                            appointment.CompanyName,
                                            meeting.Name,
-                                           appointment.CarRegistration
+                                           appointment.CarRegistration,
+                                           appointment.CheckIn,
+                                           appointment.CheckOut
                                        });
                                        
                 //Sorting  
@@ -98,6 +100,65 @@ namespace VMS.Controllers
 
         }
 
+        public IActionResult LoadGraphData()
+        {
+
+            var draw = HttpContext.Request.Form["draw"].FirstOrDefault();
+
+            // Skip number of Rows count  
+            var start = Request.Form["start"].FirstOrDefault();
+
+            // Paging Length 10,20  
+            var length = Request.Form["length"].FirstOrDefault();
+
+            // Sort Column Name  
+            var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+
+            // Sort Column Direction (asc, desc)  
+            var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+
+            // Search Value from (Search box)  
+            var searchValue = Request.Form["search[value]"].FirstOrDefault();
+
+            //Paging Size (10, 20, 50,100)  
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+
+            int skip = start != null ? Convert.ToInt32(start) : 0;
+
+            int recordsTotal = 0;
+
+
+
+            // getting all Customer data  
+            var appointmentData = (from employee in _context.Employees
+                                  select new
+                                  {
+                                      employee.Id,
+                                      employee.Name,
+                                      employee.CreatedDate
+                                  });
+
+            //Sorting  
+            if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+            {
+                //  appointmentData = appointmentData.OrderBy(sortColumn + " " + sortColumnDirection);
+            }
+            //Search  
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                appointmentData = appointmentData.Where(m => m.Name == searchValue);
+            }
+
+            //total number of rows counts   
+            recordsTotal = appointmentData.Count();
+            //Paging   
+            var data = appointmentData.Skip(skip).Take(pageSize).ToList();
+            //Returning Json Data  
+            return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data });
+
+
+
+        }
         public IActionResult GetAppointment(int Id)
         {
             var model = (from appointment in _context.Appointments.Where(x => x.Id == Id)
@@ -117,7 +178,6 @@ namespace VMS.Controllers
 
             return Json(model);
         }
-
         public IActionResult PrintPage(int Id) {
             AppointmentId = Id;
             System.Drawing.Printing.PrintDocument pd = new System.Drawing.Printing.PrintDocument();
@@ -127,16 +187,58 @@ namespace VMS.Controllers
 
             return Ok();
         }
+        public IActionResult Dashboard() 
+        {
 
-
+            return View();
+        }
         public IActionResult TestPrint(int AppointmentId)
         {
             var row = _context.Appointments.Where(x => x.Id == AppointmentId).FirstOrDefault();
 
             return View(row);
         }
+        public IActionResult MontlyVisitors()
+        {
+            DashboardDataDto model = new DashboardDataDto();
+            List<int> CheckIn = new List<int>();
+            List<int> CheckOut = new List<int>();
+            List<int> LastTenDays = new List<int>();
+            
 
-        
+            //checkIn
+            for (int i = 1; i <= 12; i++) {
+              
+                var totalCheckInForMonth = _context.Appointments.Where(x => x.CheckIn.Value.Date.Month == i).ToList().Count;
+                CheckIn.Add(totalCheckInForMonth); 
+
+            }
+
+            for (int i = 1; i <= 12; i++)
+            {
+                var totalCheckOutForMonth = _context.Appointments.Where(x => x.CheckOut.Value.Date.Month == i).ToList().Count;
+                CheckOut.Add(totalCheckOutForMonth);
+            }
+
+            for (int i = 1; i <= 10; i++) {
+                 
+                var totalCheckOutForDay = _context.Appointments.Where(x => x.CheckIn.Value.Date.Day == i 
+                && x.CheckIn.Value.Date.Year == DateTime.Now.Year).ToList().Count;
+                LastTenDays.Add(totalCheckOutForDay);
+            }
+
+            model.CheckIn = CheckIn;
+            model.CheckOut = CheckOut;
+            model.LastDaysCheckIn = LastTenDays;
+            model.TotalAppointment = _context.Appointments.Where(x => x.CheckIn != null && x.CheckOut != null).ToList().Count();
+            model.TotalEmployees = _context.Employees.ToList().Count();
+            model.TotalVisitors = _context.Appointments.ToList().Count();
+
+
+
+
+            return Ok(model);
+        }
         public async Task<IActionResult> TestPrintForImage(int AppointmentId)
         {
             var converter = new HtmlConverter();
@@ -147,7 +249,6 @@ namespace VMS.Controllers
 
 
         }
-
         void pd_PrintPage(object sender, PrintPageEventArgs e)
         {
 
@@ -179,6 +280,19 @@ namespace VMS.Controllers
             
 
 
+        }
+
+        public IActionResult ForceCheckOut(int AppointmentId)
+        {
+            var row = _context.Appointments.Where(x => x.Id == AppointmentId).FirstOrDefault();
+            if (row != null) {
+                row.CheckOut = DateTime.Now.Date;
+                _context.SaveChanges();
+
+                return Ok(row.CheckOut);
+            }
+
+            return Ok();
         }
     }
 }
---
+++ VMS/Dtos/DashboardDataDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VMS.Dtos
+{
+    public class DashboardDataDto
+    {
+        public List<int> CheckIn { get; set; }
+        public List<int> CheckOut { get; set;}
+
+        public List<int> LastDaysCheckIn { get; set; }
+
+        public int TotalVisitors { get; set; }
+        public int TotalEmployees { get; set; }
+        public int TotalAppointment { get; set; }
+
+    }
+}
